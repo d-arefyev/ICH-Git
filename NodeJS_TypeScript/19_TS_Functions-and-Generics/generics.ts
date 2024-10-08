da9dd8edbd6bdfe1be1ff21a72a702224084ed6a
Denis Arefyev <163993361+DenisArefyev@users.noreply.github.com>
2024-10-08 14:08:48 +0200

    Added 19_Functions-and-Generics

--- NodeJS_TypeScript/19_TS_Functions-and-Generics/generics.ts
+++ NodeJS_TypeScript/19_TS_Functions-and-Generics/generics.ts
@@ -8,3 +8,16 @@ foo(true);
 
 // Frontend
 // const [state, setState] = useState();
+
+function getFirstElement<T>(array: T[]): T {
+  return array[0];
+}
+
+getFirstElement([10, 42, 43, "string"]);
+
+function createPair<T, Q>(numb1: T, numb2: Q): [T, Q] {
+  return [numb1, numb2];
+}
+
+createPair(20, 22);
+createPair("str1", "str2");
